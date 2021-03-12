5c3fc325431089622d476e4a703dd5323f286e09
inugami-dev64 <jxwest@protonmail.com>
2021-03-12 23:49:29 +0200

    Remove gui_wrappers.h

---
+++ colors.h
@@ -0,0 +1,15 @@
+#ifndef __COLORS_H
+#define __COLORS_H
+
+/**************************************************
+ * This file is for setting and configuring all 
+ * the color schemes used in atm cli program
+ *************************************************/
+
+/*
+ * Preprocessor definition for enabling and disabling colorisation
+ */
+#define COLORISE 1
+
+
+#endif
